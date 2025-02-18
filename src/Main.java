e73679fc2b4a09e1ab137db8c6a02979248351e6
LJSigersmith <lancesigersmith@uconn.edu>
2025-02-18 14:53:26 -0500

    Basic Client/Server with client messaging to server

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+// to run: java Main.java ["CLIENT" or "SERVER"] [ADDRESS] [PORT]
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String opt = "SERVER";
+        String serverAddress = "localhost";
+        int port = 5001;
+
+        if (args.length > 0 && args.length < 3) { System.out.println("java Main.java [\"CLIENT\" or \"SERVER\"] [ADDRESS] [PORT]"); return; }
+        if (args.length == 3) {
+            opt = args[0];
+            serverAddress = args[1];
+            port = Integer.parseInt(args[2]);
         }
+        
+        if (opt.equals("CLIENT")) {
+            System.out.println(String.format("Starting %s on %s:%d", opt, serverAddress, port));
+            Client client = new Client(serverAddress, port);
+        } else if (opt.equals("SERVER")) {
+            System.out.println(String.format("Starting %s on %s:%d", opt, serverAddress, port));
+            Server server = new Server(port);
+        } else {
+            return;
+        }
+
     }
 }
